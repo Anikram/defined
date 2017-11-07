4454c529ccddd945fc7def4ee6cc7bae2635546d
Anikram <4nikram@gmail.com>
2017-11-07 14:56:05 +0300

    version 1

---
+++ defined.rb
@@ -0,0 +1,15 @@
+#encoding: utf-8
+a = nil
+
+if defined?(a) != nil
+  puts 'Переменная a - определена.'
+else
+  puts 'Переменная a - не определена.'
+end
+
+
+if defined?(b) != nil
+  puts 'Переменная b - определена.'
+else
+  puts 'Переменная b - не определена.'
+end
